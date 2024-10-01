8e60327595a2e7173da8d422b7501a92995f1b57
Kardi <kgland06@gmail.com>
2024-10-01 09:11:55 +0700

    fixing tailwindcss

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ function App() {
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS in React!</h1>
-      </div>
-      <div style={{ color: 'red', fontSize: '32px' }}>Hello, Inline CSS</div>
-
       <div className="h-screen flex flex-col">
         <CalendarHeader/>
         <div className="flex">

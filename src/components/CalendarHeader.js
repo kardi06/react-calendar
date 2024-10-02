75effb68c3ebe6dcfbad3bbc2193b80b41239e72
Kardi <kgland06@gmail.com>
2024-10-02 07:55:41 +0700

    calendar header

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo.png";
 
 export default function CalendarHeader() {
   return (
-    <div>CalendarHeader</div>
-  )
+    <header className="px-4 py-2 items-center flex">
+      <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
+      <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
+      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        chevron_left
+      </button>
+      <button className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        chevron_right
+      </button>
+    </header>
+  );
 }

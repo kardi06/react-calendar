e91585623031261555e7089cdffcce86b3fc132f
Kardi <kgland06@gmail.com>
2024-10-02 17:20:11 +0700

    sidebar

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -12,12 +12,16 @@ export default function CalendarHeader() {
   const handleNextMonth = () => {
     setMonthIndex(monthIndex + 1);
   }
+
+  const handleResetMonth = () => {
+    setMonthIndex(dayjs().month());
+  }
   
   return (
     <header className="px-4 py-2 items-center flex">
       <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calendar</h1>
-      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button className="border rounded py-2 px-4 mr-5" onClick={handleResetMonth}>Today</button>
       <button className="material-icons-outlined cursor-pointer text-gray-600 mx-2" onClick={handlePrevMonth}>
         chevron_left
       </button>
